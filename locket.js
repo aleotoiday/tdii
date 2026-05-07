f8b4d6a09daef02f7b670d850a1161700c18235d
aleotoiday <aleotoiday@gmail.com>
2026-05-07 18:51:42 +0700

    🌚

---
+++ locket.js
@@ -0,0 +1,5 @@
+const mapping = {
+  'Locket': ['Gold']
+};
+
+var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],aleoo=JSON.parse($response.body);aleoo.Attention="hihi";var locket02={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"2099-01-01T01:05:16Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2012-03-25T01:05:19Z",purchase_date:"2012-03-25T01:05:19Z",store:"app_store"},locket01={grace_period_expires_date:null,purchase_date:"2012-03-25T01:05:19Z",product_identifier:"com.locket02.premium.yearly",expires_date:"2099-01-01T01:05:16Z"};const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s?(locket01.product_identifier=s,aleoo.subscriber.subscriptions[s]=locket02):aleoo.subscriber.subscriptions["com.locket02.premium.yearly"]=locket02,aleoo.subscriber.entitlements[e]=locket01}else aleoo.subscriber.subscriptions["com.locket02.premium.yearly"]=locket02,aleoo.subscriber.entitlements.pro=locket01;$done({body:JSON.stringify(aleoo)});
